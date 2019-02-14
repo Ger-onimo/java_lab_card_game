b663270d86d0feafd797aabcf82da540fa354327
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-02-14 14:24:03 +0000

    Create and test deck class

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> deckOfCards;
+
+    public Deck(){
+        this.deckOfCards = new ArrayList<>();
+    }
+
+    public int countDeck(){
+        return this.deckOfCards.size();
+    }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,12 +1,22 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class DeckTest {
 
     private Deck deck;
+    private Card card;
 
     @Before
     public void before(){
         deck = new Deck();
+        card = new Card(SuitType.SPADES, RankType.ACE);
     }
 
+    @Test
+    public void canCountDeck() {
+        assertEquals(0, deck.countDeck());
+    }
 }
